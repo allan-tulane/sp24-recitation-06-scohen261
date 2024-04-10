02ea91dd1029c4b45250d9619597ea3cbc6ebd54
scohen261 <127058745+scohen261@users.noreply.github.com>
2024-04-10 23:22:11 +0000

    final

--- answers.md
+++ answers.md
@@ -1,20 +1,20 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**__Sam Cohen_______________________
+**Name:**__Andrew Zimmerman_______________________
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** W(n)= c/2 ⋅n(n−1)
 
-- **3)**
+- **3)** S(n) = c
 
-- **4)**
+- **4)** The counts increase rapidly for smaller Fibonacci numbers, but the rate of increase gradually slows down as the Fibonacci numbers become larger. 
 
-- **6)**
+- **6)** The maximum number of times that `fib_top_down(i)` will be called for any value `i` is equal to the value of `n` itself. The work and span of this function are both O(n).
 
-- **8)**
+- **8)** he maximum number of times that `fib_bottom_up(i)` will be called for any value `i` is 1. the work of this function is O(n) and the span is O(1).
--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 def fib_recursive(n, counts):
-    """
-    Return the nth Fibonacci number. 
-    counts is a list of n+1 elements, where counts[i] is incremented
-    each time fib_recursive(i, counts) is called.
-    """    
-    counts[n] += 1
+  """
+  Return the nth Fibonacci number.
+  counts is a list of n+1 elements, where counts[i] is incremented
+  each time fib_recursive(i, counts) is called.
+  """
+ 
+  counts[n] += 1 
+  if n == 0:
+      return 0
+  elif n == 1:
+      return 1
+  else:
+      
+      return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
+
     ###TODO
     pass
     
 
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+    """
+    Calculate the nth Fibonacci number using top-down dynamic programming (memoization).
+    """
+    if n == 0:
+      return 0
+    elif n == 1:
+      return 1
+
+
+    if fibs[n] != -1:
+      return fibs[n]
+
+
+    fibs[n] = fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
+    return fibs[n]
 
 
 def fib_bottom_up(n):
     ###TODO
-    pass
+  if n == 0:
+      return 0
+  elif n == 1:
+      return 1
+
+  fibs = [0] * (n + 1)  # Initialize a list to store Fibonacci numbers
+
+  # Base cases
+  fibs[0] = 0
+  fibs[1] = 1
+
+  # Compute Fibonacci numbers iteratively
+  for i in range(2, n + 1):
+      fibs[i] = fibs[i - 1] + fibs[i - 2]
+
+  return fibs[n]
 
 
 
